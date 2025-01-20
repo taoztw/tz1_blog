ddf30b87844823d91c7332543d7842e52695c8fa
tz <tztw4723@gmail.com>
2025-01-21 01:02:05 +0800

    cloudflare d1 本地开发 使用本地sqlite数据库

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,47 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+import fs from "fs";
+import path from "path";
+
+function getLocalD1DB() {
+  try {
+    const basePath = path.resolve(".wrangler");
+    const dbFile = fs
+      .readdirSync(basePath, { encoding: "utf-8", recursive: true })
+      .find((f) => f.endsWith(".sqlite"));
+
+    if (!dbFile) {
+      throw new Error(`.sqlite file not found in ${basePath}`);
+    }
+
+    const url = path.resolve(basePath, dbFile);
+    return url;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(`Error  ${err.message}`);
+    } else {
+      console.log(`Unexpected error`, err);
+    }
+  }
+}
+
 export default defineConfig({
-  out: "./.drizzle",
-  schema: "./db/schemas/schema.ts",
   dialect: "sqlite",
-  driver: "d1-http",
-  dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
-  },
-  tablesFilter: ["!_cf_KV"],
+  schema: "./db/schemas/schema.ts",
+  out: "./.drizzle",
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        driver: "d1-http",
+        dbCredentials: {
+          accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+          databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+          token: process.env.CLOUDFLARE_D1_TOKEN,
+        },
+      }
+    : {
+        dbCredentials: {
+          url: getLocalD1DB(),
+        },
+      }),
 });
